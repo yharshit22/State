79b33e5170b272026532b1ad8d08fed2863299f4
yharshit22 <136423611+yharshit22@users.noreply.github.com>
2025-03-25 14:16:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ Counter.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
+
+export default Counter;
